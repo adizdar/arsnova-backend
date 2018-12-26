5a4436fc3e8dd53cf0812504489b8891eb52db9c
adizdar <ahmed.dizdar@viison.com>
2018-12-26 23:42:43 +0100

    Add statement definition

--- src/main/java/ghost/xapi/entities/Statement.java
+++ src/main/java/ghost/xapi/entities/Statement.java
@@ -1,4 +1,27 @@
 package ghost.xapi.entities;
 
 public class Statement {
+
+	private Actor actor;
+	private Verb verb;
+	private Activity activity;
+
+	public Statement(Actor actor, Verb verb, Activity activity) {
+		this.actor = actor;
+		this.verb = verb;
+		this.activity = activity;
+	}
+
+	public Actor getActor() {
+		return actor;
+	}
+
+	public Verb getVerb() {
+		return verb;
+	}
+
+	public Activity getActivity() {
+		return activity;
+	}
+
 }
